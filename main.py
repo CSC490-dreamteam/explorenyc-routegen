82c42ec615ca26a07f85441be66719c0b0d50875
RyanCFrancis <ryan8francis@gmail.com>
2026-03-02 00:31:38 -0500

    api is accessible and solver maybe works?

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import uvicorn
+from fastapi import FastAPI
+from generator import SolverInput, SolverOutput, generate_route
+
+app = FastAPI()
+
+@app.post("/generate_route")
+def generate_route(solver_input: SolverInput) -> SolverOutput:
+    return generate_route(solver_input)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
